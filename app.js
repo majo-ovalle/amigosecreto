ac22db8310fead28a4a4efac42a89c7a0aac30f5
majo-ovalle <ramirezovallemariajose@gmail.com>
2025-08-16 01:15:39 -0600

    Agregando una funcion

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let nombres
+function agregarAmigo(){
+  let inputAmigos = document.getElementById("amigo").value;
+  if(inputAmigos.length<1){
+    alert('campo vacio');
+
+  }
+  console.log(inputAmigos);
+  
+
+}
